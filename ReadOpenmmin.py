bbc6fd7709321ddeacb95b1a4d59ca76a9faa950
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2017-12-27 15:32:29 +0900

    Delete ReadOpenmmin.py

--- ReadOpenmmin.py
+++
@@ -1,5 +0,0 @@
-
-def read(self, filename):
-    with open(filename, 'rt') as f:
-        kwds = ['integrator', 'dt', 'timestep', 'temperature', 'pressure', 'barostat', 'frition_const',
-                'hydrogen_mass', 'recstep', 'writestep',  ]    
