351c6821fd4e09b5de2a3ba1cb2390d32bc5c6a7
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-07-09 18:43:48 +0900

    Update calc_mu.py

--- myscript/calc_mu.py
+++ myscript/calc_mu.py
@@ -19,6 +19,7 @@ def print_ex(strname, num):
 
 def describe_list(list):
     print_ex("    mean", np.mean(list))
+    print_ex("  95%err", 2.0e0*np.std(list)/math.sqrt(len(list)))
     print_ex("    var", np.var(list))
     print_ex("    stdev", np.std(list))
 #    print_ex("    skew", scipy.stats.skew(list))
@@ -27,14 +28,13 @@ def describe_list(list):
     print_ex("    med", np.median(list))
     print_ex("    min", np.min(list))
 
-def calc_cumave(arrayname, linenum, rawnum, fname):
+def calc_cumave(arrayname, linenum, fname):
     maxnum = rawnum * (linenum-1) + rawnum
     datalist = [0]*maxnum
     
     for i in range(linenum):        
-        for j in range(rawnum):
-            indx = rawnum * i + j
-            datalist[indx] = arrayname[i][j]
+        indx = i
+        datalist[indx] = arrayname[i]
 
 
     with open(fname, 'wt') as f:
@@ -45,9 +45,11 @@ def calc_cumave(arrayname, linenum, rawnum, fname):
         std_indx = np.std(datalist[:indx+1])
         with open(fname, 'a+') as f:
             f.write('{0:d}\t{1:7.5f}\t{2:7.5f}\n'.format(indx, ave_indx, std_indx*2/math.sqrt(indx)))
+
+
 hists = []
 Nmax = 50
-Nmin = 39
+Nmin = 50
 indexes = range(Nmax, Nmin-1, -1)
 Ntot = len(indexes)
 for indx in indexes:
@@ -57,12 +59,15 @@ for indx in indexes:
         continue
     else:
         with open(filename, 'rt') as f:
-            mus = [map(float, line[5:].split()) for line in f]
+            mus = [float(line[5:18].split()[0]) for line in f]
 
     mus = np.array(mus)
-    rawnum = len(mus[0])
+    print(mus)
+    rawnum = len(mus)
+#    rawnum = 1
     linenum = len(mus)
-    calc_cumave(mus, linenum, rawnum, 'MELT_{0:03d}/ERmods/slvfes.tt'.format(indx))
+   
+    calc_cumave(mus, linenum, 'MELT_{0:03d}/ERmods/slvfes.tt'.format(indx))
 
     print("index:{0:03d}".format(indx))
     print("""========================= 
@@ -70,16 +75,12 @@ for indx in indexes:
 =========================""")
     describe_list(mus)
 
-    hist, bins = np.histogram(mus, range=(-8.0e0, 1.0e0), bins=100, normed=True)
+    hist, bins = np.histogram(mus, range=(-5.0e0, -2.0e0), bins=200, normed=True)
     hists.append(hist)
 
-for i in range(Ntot):
-    plt.step(bins[:-1], hists[i], where='post', label='{0:03d}'.format(Nmax-i))
+#for i in range(Ntot):
+#    plt.step(bins[:-1], hists[i], where='post', label='{0:03d}'.format(Nmax-i))
 #    plt.plot(bins[:-1], hists[i], label='{0:03d}'.format(Nmax-i))
-    with open('ermod_010conf_0500ps_rand_40000frame/slvfe_{0:03d}.dat'.format(Nmax-i), 'w+') as f:
-        for j in range(len(bins)-1):
-            f.write('{0:11.5f}\t{1:18.17f}\n'.format(bins[j], hists[i][j]))
 
-plt.legend(ncol=3)
-plt.show()
-  
+#plt.legend(ncol=3)
+#plt.show()
