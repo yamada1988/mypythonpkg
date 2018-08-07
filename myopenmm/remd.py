c2e1fea3f281bf81f7124603f20a87ff4aa4cb5e
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-08-07 15:40:47 +0900

    Update remd.py

--- myopenmm/remd.py
+++ myopenmm/remd.py
@@ -25,29 +25,8 @@ index = args[2]
 #simulation_nvt, _ = md_nvt.mdrun(simulation_em, 'nvt', index, mddir='MD/')
 
 inpf = '../inpdir/' + stage + '/npt' + index + '.inp'
+niter = 4
 # REMD (NPT)
 T_list = [453, 455, 457]
 remd = REMDConductor(T_list)
-remd.loadFile(inpf)
-nvtgro, systop = remd.preparation(sysdir='SYS/', mddir='MD/')
-simulation_npt = remd.setup(nvtgro, systop)
-print('pbcbox:', simulation_npt.system.getDefaultPeriodicBoxVectors())
-remd.mdrun(simulation_npt, 'npt', index)
-simulations = remd.spread_replicas(simulation_npt)
-tstates = remd.initialize_replicas(simulations)
-# REMD
-# equilibriation and initialization
-tstates, energys = remd.remdrun(tstates, 'npt', index, mddir='MD/', sysdir='SYS/')
-print('enes:', energys)
-b_list, p_list = remd.calc_prob(energys, 1)
-print('blist:', b_list, 'plist:', p_list)
-print(tstates)
-tstates = remd.exchange(tstates, b_list, p_list, 1, index)
-print(tstates)
-sys.exit()
-
-for niter in range(1, 2):    
-
-    b_list, p_list = remd.calc_prob(enes, niter)
-    print('blist:', b_list, 'plist:', p_list)
-    remd.exchange(sims, b_list, p_list, niter, index)
+remd.conduct(inpf, index, niter, mode='REMD', equilibriation=True)
