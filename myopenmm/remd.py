b5253e6f16cb6d95e8e9729e6039ad7d2fe5ce3a
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-08-07 10:50:32 +0900

    Update remd.py

--- myopenmm/remd.py
+++ myopenmm/remd.py
@@ -11,36 +11,43 @@ args = sys.argv
 stage = args[1]
 index = args[2]
 
-md_nvt = MDConductor()
-inpf = '../inpdir/' + stage + '/nvt' + index + '.inp'
-md_nvt.loadFile(inpf)
-sysgro, systop = md_nvt.preparation(sysdir='SYS/', mddir='MD')
-simulation_0 = md_nvt.setup(sysgro, systop)
+#md_nvt = MDConductor()
+#inpf = '../inpdir/' + stage + '/nvt' + index + '.inp'
+#md_nvt.loadFile(inpf)
+#sysgro, systop = md_nvt.preparation(sysdir='SYS/', mddir='MD')
+#simulation_0 = md_nvt.setup(sysgro, systop)
 
 # EM
-simulation_em = md_nvt.minimize(simulation_0, 'em', index,  mddir='MD/')
+#simulation_em = md_nvt.minimize(simulation_0, 'em', index,  mddir='MD/')
 
 # NVT
-simulation_nvt = md_nvt.mdrun(simulation_em, 'nvt', index, mddir='MD/')
+#nvtlog = 'MD/nvt' + index + '.log'
+#simulation_nvt, _ = md_nvt.mdrun(simulation_em, 'nvt', index, mddir='MD/')
 
-# REMD (NPT)
-T_list = [453, 463, 473, 483]#, 493, 503, 513, 523 ]
-n_replica = len(T_list)
-remd = REMDConductor(T_list)
 inpf = '../inpdir/' + stage + '/npt' + index + '.inp'
+# REMD (NPT)
+T_list = [453, 455, 457]
+remd = REMDConductor(T_list)
 remd.loadFile(inpf)
 nvtgro, systop = remd.preparation(sysdir='SYS/', mddir='MD/')
 simulation_npt = remd.setup(nvtgro, systop)
-
+print('pbcbox:', simulation_npt.system.getDefaultPeriodicBoxVectors())
+remd.mdrun(simulation_npt, 'npt', index)
+simulations = remd.spread_replicas(simulation_npt)
+tstates = remd.initialize_replicas(simulations)
 # REMD
-arglists = remd.setSims(simulation_npt, 'npt', index)
-sims, enes = [], []
+# equilibriation and initialization
+tstates, energys = remd.remdrun(tstates, 'npt', index, mddir='MD/', sysdir='SYS/')
+print('enes:', energys)
+b_list, p_list = remd.calc_prob(energys, 1)
+print('blist:', b_list, 'plist:', p_list)
+print(tstates)
+tstates = remd.exchange(tstates, b_list, p_list, 1, index)
+print(tstates)
+sys.exit()
 
-for i, args in enumerate(arglists):
-    print('index:', i)
-    j = '{0:02d}'.format(i)
-    sim, energy = remd.remdrun(args[0], args[1], args[2], args[3], args[4])
-    sims.append(sim)
-    enes.append(energy)
-print('enes:', enes)
+for niter in range(1, 2):    
 
+    b_list, p_list = remd.calc_prob(enes, niter)
+    print('blist:', b_list, 'plist:', p_list)
+    remd.exchange(sims, b_list, p_list, niter, index)
