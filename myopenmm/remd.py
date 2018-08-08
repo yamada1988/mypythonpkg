373cdba871aa10da7e143b7262b2fafc1a913dcd
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-08-08 16:18:04 +0900

    Update remd.py

--- myopenmm/remd.py
+++ myopenmm/remd.py
@@ -11,28 +11,28 @@ args = sys.argv
 stage = args[1]
 index = args[2]
 
-#md_nvt = MDConductor()
-#inpf = '../inpdir/' + stage + '/nvt' + index + '.inp'
-#md_nvt.loadFile(inpf)
-#sysgro, systop = md_nvt.preparation(sysdir='SYS/', mddir='MD')
-#simulation_0 = md_nvt.setup(sysgro, systop)
+md_nvt = MDConductor()
+inpf = '../inpdir/' + stage + '/nvt' + index + '.inp'
+md_nvt.loadFile(inpf)
+sysgro, systop = md_nvt.preparation(sysdir='SYS/', mddir='MD')
+simulation_0 = md_nvt.setup(sysgro, systop)
 
 # EM
-#simulation_em = md_nvt.minimize(simulation_0, 'em', index,  mddir='MD/')
+simulation_em = md_nvt.minimize(simulation_0, 'em', index,  mddir='MD/')
 
 # NVT
-#nvtlog = 'MD/nvt' + index + '.log'
-#simulation_nvt, _ = md_nvt.mdrun(simulation_em, 'nvt', index, mddir='MD/')
+nvtlog = 'MD/nvt' + index + '.log'
+simulation_nvt, _ = md_nvt.mdrun(simulation_em, 'nvt', index, mddir='MD/')
 
 inpf = '../inpdir/' + stage + '/npt' + index + '.inp'
 niter = 1
 # REMD (NPT)
-T_list = [453, 455, 457, 459, 461]
-remd = REMDConductor(T_list)
+T_list = [453, 456, 459, 461, 463]
+remd = REMDConductor(T_list, mode='REMD')
 # equilibriation
-remd.conduct(inpf, index, niter, mode='REMD', equilibriation=True)
+remd.conduct(inpf, index, equilibriation=True)
 
 # sampling
 inpf2 = '../inpdir/stage2' + '/npt' + index + '.inp'
 niter = 100
-remd.conduct(inpf2, index, niter, mode='REMD', equilibriation=False)
+remd.conduct(inpf2, index, niter, equilibriation=False)
