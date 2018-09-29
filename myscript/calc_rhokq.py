2861c664bfb1edbf9d481c3a434592850699e3e8
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-09-29 20:58:18 +0900

    Create calc_rhokq.py

---
+++ myscript/calc_rhokq.py
@@ -0,0 +1,105 @@
+from pytrr import GroTrrReader
+import sys
+import math
+import numpy as np
+
+#
+# Calculate rho(k,q,t) and <rho(k,q> from Gromacs trrfile.
+# This script requires pytrr. (https://github.com/andersle/pytrr)
+# install pytrr using "pip insall pytrr".
+#
+
+args = sys.argv
+fname = args[1]
+T = fname.split('_')[1].split('.')[0]
+
+pi = math.pi
+
+# Mass Parameters
+mO = 16.00 * 1.661e-27 #(kg)
+mH =  1.08 * 1.661e-27 #(kg)
+
+# Phisical Parameters
+N = 3000
+tN = 100
+L = 4.37864 #(nm) 
+#T = 200 #(K)
+M = mO + mH + mH #(u)
+kB = 1.3801e-23 #(J K^-1)
+
+# Wave number Parameters
+dk = 2.0e0*pi / L # (nm^-1)
+#vth = math.sqrt(3.0e0*kB*T/M) # (nm/fs)^-1 = (km/s)^-1
+vth = 10.0e0*pi
+alpha = 1.0
+dq = alpha * 2.0e0*pi / vth
+kN = 1
+qN = 40
+
+K = [1/math.sqrt(3) *np.array([kN*dk, kN*dk, kN*dk]) for i in range(1, kN+1)]
+Q = [1/math.sqrt(3) * np.array([i*dq, i*dq, i*dq]) for i in range(1, qN+1)]
+
+# rho_i(k,q,t,i)
+rho_i = [[[[0.0e0 for i in range(N)] for t in range(tN+1)] for q in range(qN)] for k in range(kN)]
+
+# rho(k,q,t)
+rho = [[[0.0e0 for t in range(tN+1)] for q in range(qN)] for k in range(kN)]
+
+Rs = []
+R = [0 for i in range(N)]
+Vs = []
+V = [0 for i in range(N)]
+
+icount = -1
+
+with GroTrrReader(fname) as trrfile:
+    for frame in trrfile:
+        icount += 1
+#        print(icount)
+        if icount >= tN:
+            break
+#        print(frame['step'])
+        frame_data = trrfile.get_data()
+        for i in range(N):
+#            print('molnum:', i+1)
+            iO  = 3*i 
+            iH1 = 3*i + 1
+            iH2 = 3*i + 2
+
+# Calculate single H2O molecule's center of mass (R)
+            xO  = np.array(frame_data['x'][iO])
+            xH1 = np.array(frame_data['x'][iH1])
+            xH2 = np.array(frame_data['x'][iH2])
+            R[i] = (mO * xO + mH * xH1 + mH * xH2)/ M 
+#            print('coordinate:',R[i])
+
+# Calculate single H2O molecule's velocity (V)
+            vO  = np.array(frame_data['v'][iO])
+            vH1 = np.array(frame_data['v'][iH1])
+            vH2 = np.array(frame_data['v'][iH2])
+            V[i] = (mO * vO + mH * vH1 + mH * vH2)/ M
+#            print('velocity:',V[i])
+
+# Calculate rho(k,q,t)
+            for ik,k in enumerate(K):
+                for iq,q in enumerate(Q):
+                    theta = np.dot(k,R[i]) + np.dot(q,V[i])
+                    rho_i[ik][iq][icount][i] = complex(math.cos(theta), -math.sin(theta))
+        for ik,k in enumerate(K):
+            for iq,q in enumerate(Q):
+                rho[ik][iq][icount] = 0.0e0
+                for i in range(N): 
+                    rho[ik][iq][icount] += rho_i[ik][iq][icount][i]
+                rho[ik][iq][icount] /= N
+#                print(k,q,rho[ik][iq][icount])
+
+# Calculate <rho(k,q)>
+print("##### Ensemble Averaged density ({0:3d}K) ####".format(T))
+rho_ens = [[0.0e0 for iq in range(qN)] for ik in range(kN)]
+for ik,k in enumerate(K):
+    for iq,q in enumerate(Q):
+        rho_ens[ik][iq] = 0.0e0
+        for i in range(tN):
+            rho_ens[ik][iq] += rho[ik][iq][i]
+        rho_ens[ik][iq] /= tN
+        print(k,q,rho_ens[ik][iq])
