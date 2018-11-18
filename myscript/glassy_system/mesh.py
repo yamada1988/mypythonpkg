4e226ac43ce06957503569e1dd054782f88544da
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-11-18 21:19:57 +0900

    Create mesh.py

---
+++ myscript/glassy_system/mesh.py
@@ -0,0 +1,26 @@
+import numpy as np
+import time
+
+N = 1000000
+rN = 16
+L = 32.0
+dx = L/rN
+
+def get_index(array, ind):
+    return np.where(array[:] == i)[0]
+    
+
+meshfilter = np.array([1,rN,rN**2])
+
+pos = np.random.rand(N,3)*L
+
+t0 = time.time()
+# meshid
+meshids = (pos/dx).astype(int)*meshfilter
+print('create meshid:',time.time()-t0)
+
+# get
+t1 = time.time()
+plist = [get_index(meshids, i) for i in range(rN**3)]
+print('create particle list:',time.time()-t1)
+print(plist)
