97d208cef29beabf087280b9875eb43548001be6
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-12-02 19:39:44 +0900

    Update mesh.py

--- myscript/glassy_system/mesh.py
+++ myscript/glassy_system/mesh.py
@@ -5,12 +5,18 @@ import multiprocessing
 import itertools
 import time
 
-N = 100
-rN = 3
-L =  3.0
+N = 160000
+rN = 32
+L =  32.0
 dx = L/rN
-cutoff = 1.0
+cutoff = 0.850
 n_mesh = rN**3
+N1 = int(N/2.0)
+s1 = 1.0e0
+s2 = 1.20e0
+sigma1 = s1 * np.ones(N1, dtype=np.float64)
+sigma2 = s2 * np.ones(N1, dtype=np.float64)
+sigma = np.hstack((sigma1, sigma2))
 basic_cell = np.arange(rN)
 data = np.arange(n_mesh)
 pair_list = np.zeros((rN**3,8),dtype=int)
@@ -21,7 +27,7 @@ def compute_M(data):
                       shape=(data.max() + 1, data.size))
 
 def get_indices(ind):
-    return M[ind].data 
+    return M[ind].data.tolist()
 
 
 @jit('i8[:,:](i8, i8, i8[:, :])', nopython=True)
@@ -42,25 +48,42 @@ def get_pair_list_numba(ind, rN, pairs):
     return pairs
 
 
-@jit('f8[:,:,:](f8[:,:], f8[:,:], i8, i8, f8, f8, f8[:,:,:])', nopython=True)
-def calc_dist(vec1, vec2, i1, i2, L, cutoff, array1):
-    N = vec1.shape[0]
-    M = vec2.shape[0]
-    l = 0
-    j0 = 0
-    for i in range(N):
-        if i2 == 0:
-            j0 = i+1  # avoid double count in same grid
-        for j in range(j0,M):
-            d = 0.0e0
-            for k in range(3):
-                tmp = vec1[i,k] - vec2[j,k]
-                tmp -= np.round(tmp/L)*L
-                d += np.power(tmp,2)
-            if d < cutoff:
-                array1[i1, i2, l] = np.sqrt(d)
-            l += 1
- 
+@jit('f8[:,:,:](f8[:,:], i8[:,:], i8[:,:], i8, i8, f8, f8, f8[:], f8[:,:,:])', nopython=True)
+def calc_stress(pos, address_list, pair_index, n_mesh, N1, L, cutoff, sigma, array1):
+    ep = 1.0e0
+    c2 = cutoff**2
+    for mesh_ind in range(n_mesh):
+        i1s = address_list[mesh_ind]
+        i_ = 1
+        for i1 in i1s:
+            if i1 < 0:
+                continue
+            X = pos[i1]
+            for ipair in range(8):
+                if ipair == 0:
+                    i2s = address_list[pair_index[mesh_ind][ipair]][i_:] # avoid double count in same grid
+                else:
+                    i2s = address_list[pair_index[mesh_ind][ipair]]
+
+                for i2 in i2s:
+                    if i2 < 0:
+                        continue
+                    Y = pos[i2]
+                    d = 0.0e0
+                    tmp = np.zeros(3, dtype=np.float64)
+                    for k in range(3):
+                        tmp[k] = X[k] - Y[k]
+                        tmp[k] -= np.round(tmp[k]/L)*L
+                        d += np.power(tmp[k],2)
+           
+                    if d < c2:
+                        r = np.sqrt(d)
+                        rinv = np.power(0.50e0*(sigma[i1]+sigma[i2])/r, 6)
+                        f = 4.0e0*ep*6.0e0*rinv*(-2.0e0*rinv+1.0e0)*0.50e0*tmp/r 
+                        temp_s = 0.50e0*np.outer(f, tmp)
+                        array1[mesh_ind] += temp_s
+                        array1[pair_index[mesh_ind][ipair]] += temp_s
+            i_ += 1
     return array1
 
 
@@ -71,56 +94,29 @@ t0 = time.time()
 p_address = np.sum((pos/dx).astype(int)*meshfilter,axis=1)
 print('create meshid:',time.time()-t0)
 
-# get
-# benchmark data: 32x32x32 grid, 1000000 particles
-# numpy(single): 31.76 (s)
-# cupy(single):   9.35 (s)
-# numpy(36process): 1.83 (s)
-# use scipy.sparse + list comp : 0.92 (s)
-# use scipy.sparse + mp.Pool : 0.34 (s)
 t1 = time.time()
 
 M = compute_M(p_address)
-
 p = multiprocessing.Pool()
-address_list = p.map(get_indices, data)
+X = p.map(get_indices, data)
 p.close()
+## Max size
+l = max(map(len, X))
+## Padding
+address_list = np.array(map(lambda x: x + [-1]*(l-len(x)), X), dtype=np.int64)
 print('create particle list:',time.time()-t1)
 
-pairs = np.zeros((n_mesh, 8), dtype=np.int64)
+pair_index = np.zeros((n_mesh, 8), dtype=np.int64)
 t2 = time.time()
-[get_pair_list_numba(ind, rN, pairs) for ind in data]
+[get_pair_list_numba(ind, rN, pair_index) for ind in data]
 print('create pair_list:',time.time()-t2)
 
 t3 = time.time()
-D = np.zeros((n_mesh, 8, 200), dtype=np.float64)
-for mesh_ind,address in enumerate(address_list):
-    X = pos[address]
-    for ipair, pair in enumerate(pairs[mesh_ind]):
-        Y = pos[address_list[pair]]
-        D = calc_dist(X, Y, mesh_ind, ipair, L, cutoff, D)
-
-print('calc distance:', time.time()-t3)
+stress_tensor = calc_stress(pos, address_list, pair_index, n_mesh, N1, L, cutoff, sigma, np.zeros((n_mesh, 3, 3), dtype=np.float64))
+print(stress_tensor)
+print('calc stress:', time.time()-t3)
 
 print('total:', time.time()-t0)
+
 import sys
 sys.exit()
-for id_, d_ in enumerate(D):
-    print("=== id:",id_)
-    print(address_list[id_])
-    print(pairs[id_])
-    print(d_[0])
-    print(d_[np.nonzero(d_)])
-
-
-
-print('total:', time.time()-t0)
-import sys
-sys.exit()
-#i_ = 0
-t3 = time.time()
-p = multiprocessing.Pool()
-rij = p.map(calc_dist, data)
-print('calc distance:', time.time()-t3)
-#print(i_/(N**2*0.50))
-#print(rij)
