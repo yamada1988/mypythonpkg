b2e03a8b05b90c22cbde48b726f1dc1b90cc174c
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-12-07 16:53:26 +0900

    Update wahnstrom.py

--- myscript/glassy_system/wahnstrom.py
+++ myscript/glassy_system/wahnstrom.py
@@ -1,26 +1,31 @@
-# example illustrating use of Free Energy OpenMM plugin.
-# Calculation of chemical potential of argon in a periodic box.
+# example illustrating use of on-the-fly calculation for local variables in OpenMM plugin.
+# Calculation of Wahnstrom binary solution systems
 #
 # AUTHOR
 #
-# John D. Chodera <jchodera@berkeley.edu>
+# Kazuo Yamada
 #
 # REQUIREMENTS
 #
-# numpy - Scientific computing package - http://numpy.scipy.org
+# np - Scientific computing package - http://np.scipy.org
+# h5py - Pythonic interface to the HDF5 binary data format - https://www.h5py.org
 #
 # REFERENCES
 #
 # [1] Michael R. Shirts and John D. Chodera. Statistically optimal analysis of samples from multiple equilibrium states.
 # J. Chem. Phys. 129:124105 (2008)  http://dx.doi.org/10.1063/1.2978177
+# [2] Goran Wahnstrom. Molecular-dynamics study of a supercooled two-component Lennard- Jones system.
+# Phys. Rev. A. 44:6 (1991)
 
 from __future__ import print_function
 from simtk.openmm import *
 from simtk.openmm.app import *
 from simtk.unit import *
 import mdtraj
-import numpy
+import numpy as np
+import cupy as cp
 import parmed as pmd
+import h5py
 import sys
 from sys import stdout
 
@@ -40,7 +45,7 @@ def write_pdb(filename, atoms, coordinates, box, mode="w"):
     with open(filename, mode) as xyz:
         xyz.write("HEADER    simple PDB file with {0:d} atoms\n".format(len(atoms)))
         xyz.write("CRYST1{0:9.3f}{1:9.3f}{2:9.3f}  90.00  90.00  90.00 P 1        1\n".format(box[0], box[1], box[2]))
-        for i in range(len(atoms)):
+        for i in xrange(len(atoms)):
             serial = (i+1) % 10000
             name = resName = atoms[i]
             chainID = 'A'
@@ -51,16 +56,18 @@ def write_pdb(filename, atoms, coordinates, box, mode="w"):
             x, y, z = coordinates[i]
             xyz.write("ATOM  {0:5d} {1:4s} {2:3s} {3:1s}{4:4d}{5:1s}   {6:8.3f}{7:8.3f}{8:8.3f}{9:6.2f}{10:6.2f}\n".format(serial, name, resName, chainID, resSeq, iCode, x, y, z, occupancy, tempFactor))
             
-            
+
+# number of total particles            
+nparticles = 20000 
 print(""" ###########################
    WAHNSTROM BINARY SYSTEM 
- ###########################""")
+   Num: {0:9d}
+ ###########################""".format(nparticles))
 
 # =============================================================================
 # Specify simulation parameters
 # =============================================================================
 
-nparticles =5000000 # number of total particles
 n1 = int(nparticles/2)
 n2 = n1
 atoms = ['' for i in range(nparticles)]
@@ -72,18 +79,20 @@ for particle_index in range(nparticles):
 
 # species1
 mass1 = 39.9 * amu # mass
-sigma1 = 3.4 * angstrom # Lennard-Jones sigma
+sigma1 = 0.34 * nanometers # Lennard-Jones sigma
 epsilon1 = 0.994 * kilojoule/mole # Lennard-Jones well-depth
 charge1 = 0.0e0 * elementary_charge # argon model has no charge
 
 # species2
 mass2 = 79.896 * amu # mass
-sigma2 = 4.08 * angstrom # Lennard-Jones sigma
+sigma2 = 0.408 * nanometers # Lennard-Jones sigma
 epsilon2 = 0.994 * kilojoule/mole# Lennard-Jones well-depth
 charge2 = 0.0e0 * elementary_charge # argon model has no charge
+sigma = cp.asarray(np.concatenate([(sigma1/angstrom)*np.ones(n1), (sigma2/angstrom)*np.ones(n2)]), dtype=np.float32)
+epsilon = epsilon1/ (kilojoule/mole)
 
 # Steps
-nequil_steps  = 50000 # number of dynamics steps for equilibration
+nequil_steps  =  5000 # number of dynamics steps for equilibration
 nsample_steps = 50000 # number of dynamics steps for sampling
 
 # temperature
@@ -95,11 +104,13 @@ kB = BOLTZMANN_CONSTANT_kB
 temp0 = epsilon1 / (kB*AVOGADRO_CONSTANT_NA)
 NA0 = AVOGADRO_CONSTANT_NA / mole
 m0 = mass1
+l0 = sigma1
 tau0 = ((m0/amu*1.661*10**(-27))*(sigma1/angstrom*10**-10)**2/(epsilon1/kilojoule/mole/NA0)/1000.0e0)**0.50 * 10**12 * picosecond
-print(temp0, epsilon1, tau0)
-print("Target temperature:")
+f0 = m0*l0/(tau0**2)
+e0 = epsilon1
 Tstar = float(temp)
-print(Tstar)
+print("Target temperature:", Tstar)
+kBT = kB * Tstar * kelvin * AVOGADRO_CONSTANT_NA / (kilojoule/mole) 
 
 reduced_density = 0.750 # reduced density rho*sigma^3
 temperature = Tstar * kelvin # temperature
@@ -119,21 +130,21 @@ print(sigma1, sigma2, box_edge, cutoff)
 # Build systems
 # =============================================================================
 
-# Create random initial positions. PART01
+# Create random initial positions
 gridnum = int(box_edge/(2.0*angstrom))
 print('total_divnum:',gridnum**3)
-x_ = numpy.linspace(0, box_edge/angstrom, num=gridnum)
+x_ = np.linspace(0, box_edge/angstrom, num=gridnum)
 x_ = Quantity(x_ , angstrom)
-x_, y_, z_ = numpy.meshgrid(x_, x_, x_)
-xyzmesh = numpy.vstack((x_.flatten(), y_.flatten(), z_.flatten())).T
+x_, y_, z_ = np.meshgrid(x_, x_, x_)
+xyzmesh = np.vstack((x_.flatten(), y_.flatten(), z_.flatten())).T
 
-id_ = numpy.random.choice(gridnum**3,nparticles,replace=False)
-xyz_ = numpy.round(numpy.array([xyzmesh[i]+1.0*numpy.random.rand(3) for i in id_]), 6)
+id_ = np.random.choice(gridnum**3,nparticles,replace=False)
+xyz_ = np.round(np.array([xyzmesh[i]+1.0*np.random.rand(3) for i in id_]), 6)
 positions = Quantity(xyz_ , angstrom)
 
 pbcbox = [box_edge, box_edge, box_edge]
 pbcbox = list(map(lambda x: x/angstrom, pbcbox))
-pos = numpy.array(list(map(lambda x: x/angstrom, positions)))
+pos = np.array(list(map(lambda x: x/angstrom, positions)))
 pdbf = 'SYS/system0001.pdb'
 write_pdb(pdbf, atoms, pos, pbcbox)
 pdb_ = PDBFile(pdbf)
@@ -175,19 +186,19 @@ simulation.reporters.append(StateDataReporter(stdout, 1000, time=True, step=True
 # Initiate from last set of positions.
 simulation.context.setPositions(positions)
 
-for attmpt in range(50):
-    print('attempt:{0:3d}'.format(attmpt))
+print('\n################\n Minimize Energy\n################\n')
+for attmpt in range(1000):
     state = simulation.context.getState(getEnergy=True)
     energyval = state.getPotentialEnergy()
-    print('before:', energyval)
+    print('attempt {0:6d}:'.format(attmpt))
+    print('Enrg: {0:8.5e} (kJ/mol)'.format(energyval/(kilojoule/mole)))
 
     # Minimize energy from coordinates.
-    print("minimizing..." )
-    simulation.minimizeEnergy(maxIterations=10)
-    state = simulation.context.getState(getEnergy=True)
-    energyval = state.getPotentialEnergy()
-    print('after:', energyval)
-    if energyval/(kilojoule/mole) <= 1e+3:
+    simulation.minimizeEnergy(maxIterations=2)
+    forces = simulation.context.getState(getForces=True).getForces(asNumpy=True) / (kilojoule/mole/nanometers) 
+    force_max = np.amax(forces)
+    print('Fmax: {0:8.5e} (kJ/mol/nm)'.format(force_max))
+    if force_max <= 1000.0e0:
         break
     else:
         mdh5_.close()
@@ -202,29 +213,30 @@ write_pdb(pdbf, atoms, pos, pbcbox)
 pdb_ = PDBFile(pdbf)
 system.setDefaultPeriodicBoxVectors(Vec3(box_edge, 0, 0), Vec3(0, box_edge, 0), Vec3(0, 0, box_edge))
 # Create Integrator and Context.
+recstep =  50
+fhstep  = 500
+atmstep =  10
+t_ratio = fhstep/atmstep
+logfile = 'MD/log0001_{0:03d}.txt'.format(int(temp))
 integrator = LangevinIntegrator(temperature, collision_rate, timestep)
 platform = Platform.getPlatformByName('OpenCL')
 properties = {'Precision':'single'}
-mdh5 = 'MD/md0001_{0:03d}.h5'.format(int(temp))
-mdh5_ = mdtraj.reporters.HDF5Reporter(mdh5, 1000, potentialEnergy=False, kineticEnergy=False, temperature=False, velocities=True)
 simulation = Simulation(pdb_.topology, system, integrator, platform, properties)
-simulation.reporters.append(mdh5_)
-simulation.reporters.append(StateDataReporter(stdout, 1000, time=True, step=True,
+simulation.reporters.append(StateDataReporter(logfile, recstep, time=True, step=True,
       potentialEnergy=True, temperature=True, density=True,progress=True, remainingTime=True,
       speed=True, totalSteps=nsample_steps+nequil_steps, separator='\t'))
 
 # Initiate from last set of positions.
 simulation.context.setPositions(positions)
-state = simulation.context.getState(getEnergy=True)
-energyval = state.getPotentialEnergy()
-print('before:', energyval)
-
 # Equilibrate.
-print("equilibrating...")
+print("\n###############\n Equilibration\n###############\n")
 simulation.step(nequil_steps)
 
 # run 500 ps of production simulation
-print('Running Production...')
+mdh5 = 'MD/md0001_{0:03d}.h5'.format(int(temp))
+mdh5_ = mdtraj.reporters.HDF5Reporter(mdh5, recstep, potentialEnergy=False, kineticEnergy=False, temperature=False, velocities=True)
+simulation.reporters.append(mdh5_)
+print('\n###############\n Sampling\n#################\n')
 simulation.step(nsample_steps)
 
 # output pdbfile
