b09435c89400dc5cb2dd3da25309bc442c4416ad
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-02-06 11:03:16 +0900

    Update calc_dist_gel.py

--- myscript/calc_dist_gel.py
+++ myscript/calc_dist_gel.py
@@ -3,13 +3,12 @@ import mdtraj as md
 import numpy as np
 import sys
 
-index_ = int(sys.argv[1])
-frame_ = int(sys.argv[2])
-
-Nchain = 50
-Nmol = 15841 
+fname = 'npt_8.gro'
+Nsol = 2136
+Ndmso = 8544
+Nchain = 20
 d_th = 0.50
-fname = 'SYS/DAT/npt_par{0:04d}_nopbc{1:05d}.gro'.format(index_, frame_)
+
 t = md.load(fname)
 top = t.topology
 df, b = top.to_dataframe()
@@ -23,26 +22,27 @@ df['z'] = pos[0,:,2]
 ow = df[df['name'] == 'O']
 pva_c = df[df['name'] == 'c3']
 
+print(ow)
+print(pva_c)
+
 # inter-chain
-outf = 'SYS/DAT/odist_par{0:04d}_chain{1:05d}.dat'.format(index_, frame_)
+outf = 'ocdist.dat'
 
 with open(outf, 'wt') as f:
     f.write('#index\tmol1\tchain2\tatm1\tatm2\tx1\ty1\tz1\tx2\ty2\tz2\tO-O dist\n')
 
 with open(outf, 'a+') as f:
     icount = 0
-    for i in range(Nchain+1, Nmol+1):
+    for i in range(1, Nsol+1):
         ocount = 0
         aow = ow[ow['resSeq']==i]
         pos_aow = np.array(aow[['x','y','z']]) 
         atm_aow = np.array(aow['serial'])
-        for j in range(1, Nchain+1):
-            if i == j:
-                continue
+        for j in range(Nsol+Ndmso+1, Nsol+Ndmso+Nchain+1):
             dn = pva_c[pva_c['resSeq']==j]
             pos_dn = np.array(dn[['x','y','z']])
             atm_dn = np.array(dn['serial'])
-            #print(j, pos_dn)
+            #print(j, pos_aow, pos_dn)
             for ipd, pdn in enumerate(pos_dn):
                 dr = np.abs(pos_aow - pdn)
                 #print(dr/box, np.round(dr/box))
