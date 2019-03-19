69765e9f2aca55488c03b83fd76336d08b387aa7
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-03-19 16:45:30 +0900

    Update calc_cluster.py

--- myscript/calc_cluster.py
+++ myscript/calc_cluster.py
@@ -14,12 +14,13 @@ print(colors)
 
 fname = 'hbonds_chain_1000.dat'
 box = 15.46
-r0 = 0.50e0
-nbonds_min = 30
+r0 = 0.450e0
+nbonds_min = 20
 Nmax = 8000
 
 with open(fname, 'rt') as f:
-    hbonds = [[int(line.split()[0]), int(line.split()[1]), int(line.split()[2]), float(line.split()[5]), float(line.split()[6]), float(line.split()[7])] for line in f if not line.startswith('#')]
+    hbonds = [[int(line.split()[0]), int(line.split()[1]), int(line.split()[2]), int(line.split()[3]), int(line.split()[4]), 
+               float(line.split()[5]), float(line.split()[6]), float(line.split()[7])] for line in f if not line.startswith('#')]
 
 N_hbond = len(hbonds)
 
@@ -35,9 +36,9 @@ for id_ in range(Nmax+1):
         chain_index2 = hbonds[id_][2]
     except:
         break
-    r1 = np.array(hbonds[id_][3:6])
+    r1 = np.array(hbonds[id_][5:8])
     for l in lists:
-        r2 = np.array(hbonds[l-1][3:6])
+        r2 = np.array(hbonds[l-1][5:8])
         d = np.linalg.norm(r1-r2)
         if 0.0 < d < r0:
             chain_index3 = hbonds[l-1][1]
@@ -73,7 +74,7 @@ id_c = 1
 for clust in clusters:
     for id_h in clust[0]:
         with open(outf, 'a+') as f:
-            l = '{0:3d}\t{1:5d}\t{2:4d}\t{3:4d}\t{4:7.4f}\t{5:7.4f}\t{6:7.4f}\n'.format(id_c, id_h, hbonds[id_h-1][1], hbonds[id_h-1][2],hbonds[id_h-1][3], hbonds[id_h-1][4], hbonds[id_h-1][5])
+            l = '{0:3d}\t{1:5d}\t{2:4d}\t{3:4d}\t{4:7.4f}\t{5:7.4f}\t{6:7.4f}\n'.format(id_c, id_h, hbonds[id_h-1][1], hbonds[id_h-1][2],hbonds[id_h-1][5], hbonds[id_h-1][6], hbonds[id_h-1][7])
             f.write(l)
     id_c += 1
 
@@ -82,7 +83,8 @@ new_clusters = [['',''] for i in range(id_clusts[-1]+1)]
 dels= []
 for id_c1 in range(id_clusts[-1]+1):
     print('=====cluster index:', id_c1+1)
-    for k in range(100):
+    k = 0
+    while True:
         c1 = clusters[id_c1][0]
         N0 = len(c1)
         print('=====iteration index:', k+1)
@@ -108,8 +110,9 @@ for id_c1 in range(id_clusts[-1]+1):
         print(N0, len(clusters[id_c1][0]))
         if len(clusters[id_c1][0]) == N0:
             break
+        k+=1
 
-for k in range(1000):
+while True:
     try:
         new_clusters.remove(['',''])
     except:
@@ -117,14 +120,15 @@ for k in range(1000):
 
 
 print('before')
-print(new_clusters)
+for nc in new_clusters:
+    print(nc)
 def get_unique_list(seq):
     seen = []
     return [x for x in seq if x not in seen and not seen.append(x)]
-
 new_clusters = get_unique_list(new_clusters)
 print('after')
-print(new_clusters)
+for nv in new_clusters:
+    print(nc)
 
 
 outf = 'old_hbonds_02.dat'
@@ -133,7 +137,7 @@ with open(outf, 'wt') as f:
 for id_c, clust in enumerate(new_clusters):
     for id_h in clust[0]:
         with open(outf, 'a+') as f:
-            l = '{0:3d}\t{1:5d}\t{2:4d}\t{3:4d}\t{4:7.4f}\t{5:7.4f}\t{6:7.4f}\n'.format(id_c+1, id_h, hbonds[id_h-1][1], hbonds[id_h-1][2],hbonds[id_h-1][3], hbonds[id_h-1][4], hbonds[id_h-1][5])
+            l = '{0:3d}\t{1:5d}\t{2:4d}\t{3:4d}\t{4:7.4f}\t{5:7.4f}\t{6:7.4f}\n'.format(id_c+1, id_h, hbonds[id_h-1][1], hbonds[id_h-1][2],hbonds[id_h-1][5], hbonds[id_h-1][6], hbonds[id_h-1][7])
             f.write(l)
 
 
@@ -150,12 +154,13 @@ for i in range(len(new_clusters)):
 
 outf = 'cluster_hbonds.dat'
 with open(outf, 'wt') as f:
-    f.write('#clust\thbond\tchain1\tchain2\tx\ty\tz\n')
+    f.write('#clust\thbond\tchain1\tchain2\tatm1\tatm2\tx\ty\tz\n')
 id_c = 1
 for clust in new_clusters:
     for id_h in clust[0]:
         with open(outf, 'a+') as f:
-            l = '{0:3d}\t{1:5d}\t{2:4d}\t{3:4d}\t{4:7.4f}\t{5:7.4f}\t{6:7.4f}\n'.format(id_c, id_h, hbonds[id_h-1][1], hbonds[id_h-1][2],hbonds[id_h-1][3], hbonds[id_h-1][4], hbonds[id_h-1][5])
+            l = '{0:3d}\t{1:5d}\t{2:4d}\t{3:4d}\t{4:5d}\t{5:5d}\t{6:7.4f}\t{7:7.4f}\t{8:7.4f}\n'.format(id_c, id_h, hbonds[id_h-1][1], hbonds[id_h-1][2], hbonds[id_h-1][3], hbonds[id_h-1][4],
+                                                                                        hbonds[id_h-1][5], hbonds[id_h-1][6], hbonds[id_h-1][7])
             f.write(l)
     id_c += 1
 
@@ -171,28 +176,36 @@ for ic1, c1 in enumerate(new_clusters):
         if list(set(c2[1]) & set(c1[1])) != []:
             print(ic1+1, ic2+1, 'bonded')
             linked_nodes[ic1] += 1
+            linked_nodes[ic2] += 1
 
 outf = 'clusters.dat'
 with open(outf, 'wt') as f:
-    f.write('#clust\tbonds\tk\tlinks\tx\ty\tz\n')
+    f.write('#clust\tbonds\txsi\tk\tlinks\tx\ty\tz\n')
 
 pos_clust = [[0.0e0, 0.0e0, 0.0e0] for i in range(len(new_clusters))]
 with open(outf, 'at') as f:
     for ic, clust in enumerate(new_clusters):
         N = len(clust[0])
         for id_h in clust[0]:
-            pos_clust[ic][0] += hbonds[id_h-1][3]
-            pos_clust[ic][1] += hbonds[id_h-1][4]
-            pos_clust[ic][2] += hbonds[id_h-1][5]
+            pos_clust[ic][0] += hbonds[id_h-1][5]
+            pos_clust[ic][1] += hbonds[id_h-1][6]
+            pos_clust[ic][2] += hbonds[id_h-1][7]
         pos_clust[ic][0] /= N
         pos_clust[ic][1] /= N
         pos_clust[ic][2] /= N
-        f.write('{0:4d}\t{1:3d}\t{2:2d}\t{3:3d}\t{4:7.4f}\t{5:7.4f}\t{6:7.4f}\n'.format(ic+1, len(clust[0]), len(clust[1]), linked_nodes[ic], pos_clust[ic][0], pos_clust[ic][1], pos_clust[ic][2]))
+        r = 0.0e0
+        for id_h in clust[0]:
+            for i in range(5,8):
+                r0 = abs(hbonds[id_h-1][i] - pos_clust[ic][i-5])
+                r = max([r, r0]) 
+        r *= 2.0e0
+        f.write('{0:4d}\t{1:3d}\t{2:5.3f}\t{3:2d}\t{4:3d}\t{5:7.4f}\t{6:7.4f}\t{7:7.4f}\n'.format(ic+1, len(clust[0]), r, len(clust[1]), linked_nodes[ic], 
+                pos_clust[ic][0], pos_clust[ic][1], pos_clust[ic][2]))
 
 
 # Plot backbones
 fig = pyplot.figure(figsize=(12,8))
-ax = Axes3D(fig)
+ax = fig.gca(projection='3d')
 
 ax.set_xlabel("X-axis")
 ax.set_ylabel("Y-axis")
@@ -208,31 +221,33 @@ for i in range(Nc):
     y = pos_clust[i][1]
     z = pos_clust[i][2]
     print('index:',i, x, y, z)
-    ax.plot([x], [y], [z], "*", color=colors[i%50], ms=10)
+    ax.plot([x], [y], [z], "o", color=colors[i%50], ms=25)
+    ax.text(x-0.20, y-0.20, z-0.20, '{0:2d}'.format(i+1), color='white' )
 
 for ic1, c1 in enumerate(new_clusters):
-    for ic2 in range(ic1+1, Nc):
+    for ic2 in range(ic1+1, Nc+1):
         x = []
         y = []
         z = []
         if ic2 >= Nc-1:
             break 
-        print(ic1+1,ic2+1)
+        print(ic1+1, ic2+1)
         c2 = new_clusters[ic2+1]
         if list(set(c2[1]) & set(c1[1])) != []:
             print(ic1+1, ic2+1, 'bonded')
-            print(pos_clust[ic1+1], pos_clust[ic2+1])
-            a = np.array(pos_clust[ic1+1])
-            b = np.array(pos_clust[ic2+1])
+            print(pos_clust[ic1], pos_clust[ic2])
+            a = np.array(pos_clust[ic1])
+            b = np.array(pos_clust[ic2])
             d = np.linalg.norm(a-b)
-            if d > box * 0.60e0:
+            if d > box * 0.50e0:
                 continue
-            x.append(pos_clust[ic1+1][0])
-            x.append(pos_clust[ic2+1][0])
-            y.append(pos_clust[ic1+1][1])
-            y.append(pos_clust[ic2+1][1])
-            z.append(pos_clust[ic1+1][2])
-            z.append(pos_clust[ic2+1][2]) 
+            x.append(pos_clust[ic1][0])
+            x.append(pos_clust[ic2][0])
+            y.append(pos_clust[ic1][1])
+            y.append(pos_clust[ic2][1])
+            z.append(pos_clust[ic1][2])
+            z.append(pos_clust[ic2][2]) 
             ax.plot(x,y,z, color='k', linestyle=':', linewidth=1)
-
+            
+pyplot.savefig('clust.eps')
 pyplot.show()
