e4936abe708cfdb2a01b7959ea777ca6ca8cb006
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-05-24 16:01:17 +0900

    Update calc_rdf_xtc.py

--- myscript/rdf_com/calc_rdf_xtc.py
+++ myscript/rdf_com/calc_rdf_xtc.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 args = sys.argv
  
-MassTable = {'C':12.0,'O':24.0,'Os':24.0,'VS':1.008,'H':1.008}
+MassTable = {'C':12.0,'O':16.0,'Os':24.0,'VS':1.008,'H':1.008}
 target = 100
 dr = 0.020 #nm
 Lstd = 12.0
