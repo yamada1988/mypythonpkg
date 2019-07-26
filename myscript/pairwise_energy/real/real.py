4a406480e823cacc5434ffb1ff1adfdefe98deb1
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-07-26 10:17:22 +0900

    Update real.py

--- myscript/pairwise_energy/real/real.py
+++ myscript/pairwise_energy/real/real.py
@@ -2,7 +2,7 @@ import ctypes
 import numpy as np
 
 def real_fortran(i, j, k, box, rcut, kappa, charge, A, e_real):
-    f = np.ctypeslib.load_library("librealfort.so", ".")
+    f = np.ctypeslib.load_library("liburealfort.so", ".")
     f.ureal_.argtypes = [
         ctypes.POINTER(ctypes.c_int32),
         ctypes.POINTER(ctypes.c_int32),
@@ -23,14 +23,15 @@ def real_fortran(i, j, k, box, rcut, kappa, charge, A, e_real):
     rcut = ctypes.byref(ctypes.c_double(rcut))
     kappa = ctypes.byref(ctypes.c_double(kappa))
 
-    f.ureal_(fk, fj, fi, box, rcut, kappa, charge, A, e_real)
+    f.ureal_(fi, fj, fk, box, rcut, kappa, charge, A, e_real)
 
-i =  5
+i =  2000
 a =  4
 v =  3
-box = 3.0
+box = 5.0
 rcut = 1.20
-kappa = 1.0 
+kappa = 1.6
+print(kappa)
 
 charge = np.zeros((i, a))
 e_real = np.zeros((i,i))
@@ -41,15 +42,20 @@ for im in range(i):
 A = box*np.random.rand(i, a, v)
 
 
-for ii in range(i):
-    for ij in range(a):
-        for ji in range(ii+1,i):
-            for jj in range(a):
-                r = A[ii,ij] - A[ji,jj]
-                r -= box*np.round(r/box)
-                print(ii, ji, ij, jj, r)
+#for ii in range(i):
+#    for ij in range(a):
+#        for ji in range(ii+1,i):
+#            for jj in range(a):
+#                r = A[ii,ij] - A[ji,jj]
+#                r -= box*np.trunc(r/(box/2.0))
+#                print(ii, ji, ij, jj, np.sqrt(np.sum(r**2)))
+
+import time
+t0 = time.time()
 
 real_fortran(i, a, v, box, rcut, kappa, charge, A, e_real)
+
+print(time.time()-t0)
 
 
 print(e_real)
