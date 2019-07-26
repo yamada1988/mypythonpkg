16327f881f5694cfaea2ce138633d1203dbe67c5
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-07-26 10:19:20 +0900

    Update lj.py

--- myscript/pairwise_energy/lj/lj.py
+++ myscript/pairwise_energy/lj/lj.py
@@ -22,9 +22,14 @@ def lj_fortran(i, j, k, box, rcut, sigma, eps, A, e_lj):
     box = ctypes.byref(ctypes.c_double(box))
     rcut = ctypes.byref(ctypes.c_double(rcut))
 
-    f.lj_(fk, fj, fi, box, rcut, sigma, eps, A, e_lj)
+    f.lj_(fi, fj, fk, box, rcut, sigma, eps, A, e_lj)
 
-i =  5
+
+
+
+
+
+i =  1000
 a =  4
 v =  3
 box = 3.0
@@ -40,15 +45,17 @@ for im in range(i):
 
 A = box*np.random.rand(i, a, v)
 
-for ii in range(i):
-    for ij in range(a):
-        for ji in range(ii+1,i):
-            for jj in range(a):
-                r = A[ii,ij] - A[ji,jj]
-                r -= box*np.round(r/box)
-                print(ii, ji, ij, jj, r)
+#for ii in range(i):
+#    for ij in range(a):
+#        for ji in range(ii+1,i):
+#            for jj in range(a):
+#                r = A[ii,ij] - A[ji,jj]
+#                r -= box*np.round(r/box)
+#                print(ii, ji, ij, jj, r)
 
+import time
+t0 = time.time()
 lj_fortran(i, a, v, box, rcut, sigma, eps, A, e_lj)
-
+print('fortran:', time.time()-t0)
 
 print(e_lj)
