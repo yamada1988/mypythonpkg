a7ce61913429fa81e032de93c5269407938b7d37
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2020-07-22 16:44:28 +0900

    Update structure_factor.f90

--- myscript/glassy_system/structure_factor.f90
+++ myscript/glassy_system/structure_factor.f90
@@ -4,81 +4,97 @@
 !
 
 program example
-
-    ! 1. Use the xdr interface
+    !$ use omp_lib
     use xdr, only: xtcfile
-
-
-    ! 2. Declare a variable of type xtcfile
-    type(xtcfile) :: xtcf
+    implicit none
+    type(xtcfile) :: xtc
     real(kind=8), parameter :: pi = 3.141592653589793238462643383
     real(kind=8), parameter :: sgm = 0.60 !nm
-    real(kind=8), parameter :: dk = 0.050
-    real(kind=8), allocatable :: pos(:,:), S(:)
-    real(kind=8) :: box_dim, xr(3), k, sinkr, diff
-    integer, parameter :: Nk = 40
-    integer :: npos, i, j, it, nt, ik
+    real(kind=8), parameter :: dk = 1.0, dr = 0.00020, k0 = 0.000010
+    real(kind=8), allocatable :: pos(:,:), S(:), S0(:), r(:), sinqr(:)
+    real(kind=8) :: box_dim, xr(3), k, sinkr, diff, r_max, rho, r_
+    integer, parameter :: Nk = 20
+    integer :: i, j, it, nt, ik, nhist
+    character(50) :: fname, dummy
+    integer :: npos
+    integer, parameter :: Ntmax = 10, dt = 1
+
+    call xtc % init("../../../MD/md/short.xtc")
+    npos = xtc % NATOMS / 4  !number of water molecules (NATOMS is obtained after calling init)
+    allocate(pos(3, npos))
+    call xtc % read
+
+    ! box information cannot be obtained until at least one read call
+    box_dim = xtc % box(1,1)
 
     allocate(S(Nk))
-    
-    ! 3. Initialize it with the names of xtc files you want to read in and write out
-    call xtcf % init("../../../MD/md/md.xtc")
-    npos = xtcf % NATOMS / 4  !number of water molecules (NATOMS is obtained after calling init)
-    allocate(pos(3, npos))
+    allocate(S0(Nk))
+    allocate(sinqr(npos))
+    S = 0.0E0
+    S0 = 0.0E0 
 
-
-
-    ! 4. Read in each configuration. Everything is stored in the xtcfile type (precision, time,
-    !    step, no of atoms, positions, etc.). Look in the xtc module for more details.
-    !    You can save the positions in the loop for your calculations in another array, or 
-    !    do your calculations after each read.
-
-    call xtcf % read
     it = 0
     nt = 0
-    Sk = 0.0E0
-    do while ( xtcf % STAT == 0 )
 
-        if ( it > 20000 ) then
-            exit
-        end if
-        if ( mod(it, 5000) /= 0) then 
-        call xtcf % read
-        it = it + 1
-        else
-        ! Just an example to show what was read in
-        write(*,'(a,f12.6,a,i0)') " Time (ps): ", xtcf % time, "  Num. Atoms: ", xtcf % NATOMS
-        pos = xtcf % pos(:, 1:xtcf % NATOMS:4)  !get the position of OW (every 1st atom)
-        box_dim = xtcf % box(1,1)
+    r_max = box_dim / 2.0E0
+    nhist = ceiling(r_max / dr)
+    rho = npos / (box_dim**3.0E0)
+    allocate(r(nhist))
+    r = [((i - 0.50E0) * dr, i = 1, nhist)]  !set r-scales as the middle points
+    
 
+
+    do it=0,Ntmax,dt
+        pos = xtc % pos(:, 1:xtc % NATOMS:4)  !get the position of OW (every 3rd atom)
+
+        !$omp parallel private(xr, diff, sinkr)
+        !$omp do 
         do ik = 1, Nk
-          k = ik*dk
-          S(ik) = S(ik) + 1.0 / npos
+          k = (ik-1)*dk + k0
           do i = 1, npos
             do j = i+1, npos
               xr = pos(:, i) - pos(:, j)
               xr = xr - box_dim * nint(xr/box_dim)  !wrap distance (periodic boundary condition)
               diff = sqrt(sum(xr**2.0))
-              sinkr = sin(k*diff)/(k*diff)
-              S(ik) = S(ik) + 2.0 * sinkr/(npos**2.0)
-              ! write (*,*) i, j, sinkr
+              if ( diff <= r_max ) then
+                sinkr = sin(k*diff)/(k*diff)
+                !r_ = (ceiling(diff / dr) - 0.50E0) * dr
+                !sinkr = sin(k*r_)/(k*r_)
+                S(ik) = S(ik) + 2.0E0 * sinkr/npos
+              end if
             end do
           end do 
         end do
-
-        call xtcf % read
-        it = it + 1
-        nt = nt + 1
-        end if
+        !$omp end do
+        !$omp end parallel
+        call xtc % read 
     end do
 
-    do ik = 1, Nk
-      k = ik * dk
-      S(ik) = S(ik) / nt
-      write (*,*) k, S(ik)
-    end do
-   
     ! 5. Close the file
-    call xtcf % close
+    call xtc % close
+
+    nt = int(Ntmax/dt) + 1
+    S = S / real(nt)
+    S = S + 1.0E0
+
+    write (*,*) nt, nhist
+    do ik=1, Nk
+      k = (ik-1)*dk + k0
+      S0(ik) = S0(ik) + 0.50E0*dr*rho*4.0*pi*r(1)**2.0E0*sin(k*r(1))/(k*r(1))
+      S0(ik) = S0(ik) + 0.50E0*dr*rho*4.0*pi*r(nhist)**2.0E0*sin(k*r(nhist))/(k*r(nhist))
+      do i=2, nhist-1
+        r_ = r(i)
+        S0(ik) = S0(ik) + dr*rho*4.0*pi*r_**2.0E0*sin(k*r_)/(k*r_)
+      end do
+    end do
+ 
+
+    open (17,file='Sk.dat')
+    do ik = 1, Nk
+      k = (ik-1)*dk + k0
+      write (17, '(f10.6,2X,f10.4,2X,f10.4,2X,f10.4)') k, S(ik), S0(ik), S(ik)-S0(ik)
+    end do
+    close(17)
+   
 
 end program example
