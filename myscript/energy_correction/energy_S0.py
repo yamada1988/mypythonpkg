451b1b9a8a3a98116fec8f9bfc86855eb1e04d3b
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2021-12-10 15:25:15 +0900

    Create energy_S0.py

---
+++ myscript/energy_correction/energy_S0.py
@@ -0,0 +1,33 @@
+from myopenmm import *
+import mdtraj as md
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from sys import stdout
+import sys
+import os
+import parmed as pmd
+
+args = sys.argv
+stage = args[1]
+index = args[2]
+gpuindex = args[3]
+
+parm = pmd.load_file('SYS/topol_S0.top')
+
+md_npt = MDConductor(index)
+inpf = '../inpdir/' + stage + '/S0.inp'
+
+# Production Run
+md_npt.loadFile(inpf)
+sysgro, systop = md_npt.preparation(sysdir='SYS/', mddir='MD')
+simulation_npt = md_npt.setup(sysgro, systop, deviceindex=gpuindex)
+#context = simulation_npt.context
+#print(pmd.openmm.energy_decomposition(parm, context))
+
+
+
+print("total:")
+state = simulation_npt.context.getState(getEnergy=True)
+energyval = state.getPotentialEnergy()
+print(energyval)
