ac319d516e6029294dcbe686c5b62a5245766296
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2022-01-06 15:18:43 +0900

    Update rerun_openmm_ghost.sh

--- myscript/energy_correction/rerun_openmm_ghost.sh
+++ myscript/energy_correction/rerun_openmm_ghost.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PJM -N "rogh16P39replicanum"
+#PJM -N "rgB16A034replicanum"
 #PJM -L "rscunit=ito-b"
 #PJM -L "rscgrp=hp-g-4"
 #PJM -L "vnode=1"
 #PJM -L "vnode-core=36"
-#PJM -L "elapse=01:30:00"
+#PJM -L "elapse=02:00:00"
 #PJM -X
 #PJM -e "OUT/reruns.e.replicanum"
 #PJM -o "OUT/reruns.o.replicanum"
@@ -14,8 +14,8 @@ module load cuda/8.0
 module load intel/2017
 
 i=replicanum
-m=16
-n1=`expr $m \* $i - $m - 1`
+m=10
+n1=`expr $m \* $i - $m + 1`
 n2=`expr $n1 + $m - 1`
 
 
@@ -25,13 +25,13 @@ mkdir DAT
 
 date
 
+gpuindex=0
 for n in `seq $n1 $n2`
 do
 
 echo $n
 num=`printf %04d $n`
-gpuindex=0
-for ind in "_ghost" 
+for ind in "_ghost" "_B0" 
 do
 
   g=`echo $(( $gpuindex % 4 ))`
