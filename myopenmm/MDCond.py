321cbef78b9bc0219094bc28c409b986252299f1
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2022-02-01 15:47:51 +0900

    LangevinIntagrator -> LanevinMiddleIntegrator
    "LangevinIntegrator is very similar to LangevinMiddleIntegrator, but it uses a different discretization of the Langevin equation. LangevinMiddleIntegrator tends to produce more accurate configurational sampling, and therefore is preferred for most applications. Also note that LangevinIntegrator, like LangevinMiddleIntegrator, is a leapfrog integrator, so the velocities are offset by half a time step from the positions."
    from http://docs.openmm.org/latest/userguide/application/02_running_sims.html#simulation-parameters

--- myopenmm/MDCond.py
+++ myopenmm/MDCond.py
@@ -528,7 +528,7 @@ class MDConductor:
 
         if self.integrator == 'Langevin':
             print('set Langevin Integrator...')
-            integrator = LangevinIntegrator(temperature, fric_const, dt)
+            integrator = LangevinMiddleIntegrator(temperature, fric_const, dt)
         elif self.integrator == 'Brownian':
             print('set Brownian Integrator...')
             integrator = BrownianIntegrator(temperature, fric_const, dt)
