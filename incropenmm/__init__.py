3e86b1b9a936eae1918032f62be9741166e7be54
k.ymada <k.yamada@cheng.es.osaka-u.ac.jp>
2022-04-27 18:06:42 +0900

    Remove unnecessary import statements.

--- incropenmm/__init__.py
+++ incropenmm/__init__.py
@@ -2,8 +2,5 @@ from __future__ import print_function, division
 #from .MDCond import MDConductor
 from .mythreading import MyThread
 from .MDCond import *
-#from .MDCond_FEP import MDConductor_FEP
-#from .MDCond_FEP_multirun import MDConductor_FEP_multirun
-#from .MDCond_legacy import MDConductor_legacy
 from .xtcreporter import XTCReporter
 from .trrreporter import TRRReporter
