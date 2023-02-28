0e558fc8e34ffbd8da0dc6ef524f18e2cf210eab
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2023-02-28 09:37:41 +0900

    load ==> iterload
    for avoiding memory error

--- myscript/calc_energy/rerun.py
+++ myscript/calc_energy/rerun.py
@@ -1,5 +1,4 @@
-# Compute (U(t)-W(t)) described in Yamada(blend.pdf, Appendix C(scheme 2) )
-from myopenmm import *
+from incropenmm import *
 import mdtraj as md
 from simtk.openmm.app import *
 from simtk.openmm import *
@@ -12,41 +11,42 @@ import parmed as pmd
 args = sys.argv
 stage = args[1]
 index = args[2]
-
+gpuindex = args[3]
 
 md_npt = MDConductor(index)
-inpf = '../inpdir/' + stage + '/water.inp'
+inpf = '../inpdir/' + stage + '/npt_ghost.inp'
 
 # Production Run
 md_npt.loadFile(inpf)
 sysgro, systop = md_npt.preparation(sysdir='SYS/', mddir='MD')
-simulation_npt = md_npt.setup(sysgro, systop)
+simulation_npt = md_npt.setup(sysgro, systop, deviceindex=gpuindex)
+
 context = simulation_npt.context
 
 
-ofname = 'energy' + index + '_openmm_trr.dat'
-#fname = 'MD/md' + index + '.xtc' 
-fname = 'MD/mdtrr' + index + '.trr'
-#fname = 'MD/copies' +index + '.trr'
-
+ofname = 'DAT/npt' + index + '_ghost.dat'
+fname = 'MD/npt' + index + '.xtc'
+#fname = 'MD/energy' + index + '.trr'
 with open(ofname, 'wt') as f:
     f.write('# step\tEnergy(kJ/mol)\n')
 
-# load trajcetory
-traj = md.load(fname,top=sysgro)
-# read box informations
-ucell_vs = traj.unitcell_vectors
+# load trajcetory in chunk_traj
+i = 1
+for chunk_traj in md.iterload(fname,chunk=500,top=sysgro):
+    # read box informations
+    ucell_vs = chunk_traj.unitcell_vectors
 
-for i in range(len(traj)):
-    # read and set positions
-    context.setPositions(traj.openmm_positions(i))
-    # set periodicbixvectors
-    ucell_v = ucell_vs[i]
+    for j in range(len(chunk_traj)):
+        # read and set positions
+        context.setPositions(chunk_traj.openmm_positions(j))
+        # set periodicbixvectors
+        ucell_v = ucell_vs[j]
 
-    context.setPeriodicBoxVectors(ucell_v[0],ucell_v[1],ucell_v[2])
-    state = context.getState(getEnergy=True)
-    energyval = state.getPotentialEnergy()
-    energyval /= kilojoules/mole
-    print(i, energyval)
-    with open(ofname, 'a+') as f:
-        f.write('{0:04d}\t{1:10.3f}\n'.format(i, energyval)) 
+        context.setPeriodicBoxVectors(ucell_v[0],ucell_v[1],ucell_v[2])
+        state = context.getState(getEnergy=True)
+        energyval = state.getPotentialEnergy()
+        energyval /= kilojoules/mole
+        #print(i, energyval)
+        with open(ofname, 'a+') as f:
+            f.write('{0:04d}\t{1:10.3f}\n'.format(i, energyval))    
+        i += 1
