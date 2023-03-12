8909e1932938d79fdab65ea9025473d4fe8569d4
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2023-03-12 12:39:56 +0900

    Create sysintex.py

---
+++ myscript/make_Sltconfs/sysintex.py
@@ -0,0 +1,32 @@
+import sys
+
+args = sys.argv
+fname = args[1]
+
+with open(fname, 'rt') as f:
+    lines = [line.strip() for line in f]
+
+numatm = 477
+index0 = 7
+index1 = numatm + index0 - 1
+
+resnames = []
+resnums = []
+resindices = []
+k0 = 1
+for i in range(index0, index1):
+    #print(i,lines[i])
+    li = lines[i].split()
+    k = int(li[2])
+    #print(li[3],k,k0)
+    if k == k0:
+        resnames.append(li[3])
+        resnums.append(k)
+        resindices.append(int(li[0])-1)
+        k0 += 1
+
+#print(resnames, resnums, resindices)
+
+for i0, ri in enumerate(resindices):
+    l = "{0:3d}\t".format(i0) + resnames[i0] + "\t{0:3d}".format(ri)
+    print(l)
